d4e52c242d8f390478e2b59f579e905e00e53dae
Danny Ngo <danny.ngo@city.ac.uk>
2022-05-31 13:41:49 +0100

    update to new data model

--- src/UGform/components/Profiles.js
+++ src/UGform/components/Profiles.js
@@ -11,8 +11,8 @@ const Profiles = ({profiles, sIndustry}) =>(
 					<div className='student-profiles__profile' key={i}>
 						<div className="student-profiles__profile__img">
 							{
-								profile.metaData.K ? 
-									<a href={profile.liveUrl}><img src={profile.metaData.K} alt={profile.title}/></a>
+								profile.listMetaData.K ? 
+									<a href={profile.liveUrl}><img src={profile.listMetaData.K[0]} alt={profile.title}/></a>
 									:
 									<a href={profile.liveUrl}><FaUser/></a>
 							}
@@ -20,20 +20,20 @@ const Profiles = ({profiles, sIndustry}) =>(
 						<div className="student-profiles__profile__details">
 							<div className="student-profiles__profile__details__title"><a href={profile.liveUrl}>{profile.title}</a></div>
 							{
-								profile.metaData.I ? 
-									<p><span className="filterCat">Industry:</span> <span className="firstLetterCap">{profile.metaData.I}</span></p>
+								profile.listMetaData.I ? 
+									<p><span className="filterCat">Industry:</span> <span className="firstLetterCap">{profile.listMetaData.I[0]}</span></p>
 									:
 									null
 							}
 							{
-								profile.metaData.P ? 
-									<p><span className="filterCat">Programme:</span> <span className="firstLetterCap">{profile.metaData.P}</span></p>
+								profile.listMetaData.P ? 
+									<p><span className="filterCat">Programme:</span> <span className="firstLetterCap">{profile.listMetaData.P[0]}</span></p>
 									:
 									null
 							}
 							{
-								profile.metaData.N ? 
-									<p><span className="filterCat">Nationality:</span> <span className="firstLetterCap">{profile.metaData.N.replace(';','/')}</span></p>
+								profile.listMetaData.N ? 
+									<p><span className="filterCat">Nationality:</span> <span className="firstLetterCap">{profile.listMetaData.N[0].replace(';','/')}</span></p>
 									:
 									null
 							}
